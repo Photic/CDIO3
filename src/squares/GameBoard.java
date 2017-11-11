199123183c3d32bcfbc245265bfce5610cfdaa5f
Photic <steen_home@hotmail.com>
2017-11-11 13:00:32 +0100

    Mere kode
    Tilføjet klasser, og ændret i de gamle.

--- src/squares/GameBoard.java
+++ src/squares/GameBoard.java
@@ -3,20 +3,21 @@ package squares;
 public class GameBoard {
 	
 	Square[] squares = new Square[32];
+	Start start = new Start("Start", 10);
+	Chance chance = new Chance();
+	Territory territory = new Territory("Ballon Stand", 10, "Lilla");
+	
+	
+	public GameBoard()
+	{	
+		squares[0] = start;
+		
+	}
 	
 	public Square getFierld(int a)
 	{
 		return squares[a];
 	}
 	
-	public void createBoard()
-	{
-		for (int i = 0; i < squares.length; i++) 
-		{
-			
-			
-			
-		}
-	}
 
 }
