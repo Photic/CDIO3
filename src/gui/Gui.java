060093221d63606e49aed278233d9335a92c7349
Mathias Milter Liboriussen <mathiasmilter@gmail.com>
2017-11-11 13:26:26 +0100

    GUI stuff

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -7,6 +7,7 @@ import desktop_fields.Field;
 import desktop_fields.Street;
 import desktop_resources.GUI;
 import monopoly_junior.Game;
+import squares.GameBoard;
 
 public class Gui {
 	
@@ -16,27 +17,27 @@ public class Gui {
 	 * @param game
 	 * An instance of the player class
 	 */
-	public void defineGUI(Game game) {
-		Field[] fields = new Field[1];
+	public void defineGUI(Game game, GameBoard gameboard) {
+		Field[] fields = new Field[2];
+		
+		//gameboard.getField(0).getName()
 		fields[0] = new Street.Builder()
-				.setTitle("The Rulebook!")
-				.setDescription("The Tower (+250)\n"
-						+ "The Crater (-100)\n"
-						+ "The Palace Gates (+100)\n"
-						+ "The Cold Desert (-20)\n"
-						+ "The Walled city (+180)\n"
-						+ "The Monastery (+0)\n"
-						+ "The Black cave (-70)\n"
-						+ "Huts in the mountain (+60)\n"
-						+ "The Werewall (-80) - Extra turn\n"
-						+ "The pit(-50)\n"
-						+ "The Goldmine (+650)")
+				.setTitle("Start")
+				.setDescription("Recieve 4$ for passing.")
 				.setSubText("")
-				.setBgColor(Color.green)
+				.setBgColor(new Color(208, 251, 230))
 				.build();
+		
+		fields[1] = new Street.Builder()
+				.setTitle("Chance")
+				.setDescription("Take a chance card")
+				.setSubText("")
+				.setBgColor(new Color(208, 251, 230))
+				.build();
+		
+		
+		
 		GUI.create(fields);
-		GUI.addPlayer(game.getP1().getName(), game.getP1().getScore());
-		GUI.addPlayer(game.getP2().getName(), game.getP2().getScore());
 	}
 
 }
