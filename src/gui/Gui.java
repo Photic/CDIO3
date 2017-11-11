c43114a59bf5e1a53051d8e2d98d5d065a77e1dd
Mathias Milter Liboriussen <mathiasmilter@gmail.com>
2017-11-11 16:31:24 +0100

    tilføjet en spiller som test

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -3,6 +3,7 @@ package gui;
 
 import java.awt.Color;
 
+import desktop_codebehind.Car;
 import desktop_fields.Field;
 import desktop_fields.Street;
 import desktop_resources.GUI;
@@ -23,7 +24,6 @@ public class Gui {
 		
 		
 		
-		
 		for (int i = 0; i <= gameboard.getSize()-1; i++) {
 			fields[i] = new Street.Builder()
 					.setTitle(gameboard.getField(i).getNavn())
@@ -34,13 +34,17 @@ public class Gui {
 		}
 		
 		
-		
-		
-		
-		
-		
-		
+
 		GUI.create(fields);
+        Car car = new Car.Builder()
+                .typeRacecar()
+                .primaryColor(Color.BLUE)
+                .secondaryColor(Color.RED)
+                .patternDiagonalDualColor()
+                .build();
+            GUI.addPlayer("Mathias", 30000, car);
+            
+            GUI.displayChanceCard("You have to pay rent. 5$");
 	}
 
 }
