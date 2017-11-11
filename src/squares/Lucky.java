e62a6f1704cdd5d1c464b8ab7a4bb195a05f0de2
Mathias Milter Liboriussen <mathiasmilter@gmail.com>
2017-11-11 12:37:38 +0100

    lucky klassen

--- src/squares/Lucky.java
+++ src/squares/Lucky.java
@@ -2,4 +2,12 @@ package squares;
 
 public class Lucky extends Square {
 
+	private int prize;
+	
+	public Lucky(String navn, int prize) {
+		super(navn);
+		this.prize = prize;
+		// TODO Auto-generated constructor stub
+	}
+
 }
