d716d8acbc358e8da1c5de2671dd7eb1dfd3db24
Photic <steen_home@hotmail.com>
2017-11-11 13:26:07 +0100

    Gameboard continue.

--- src/squares/GameBoard.java
+++ src/squares/GameBoard.java
@@ -7,8 +7,14 @@ public class GameBoard {
 	Chance chance = new Chance("Chance");
 	Territory territoryPurple1 = new Territory("BALLOON STAND", 10, "Purple");
 	Territory territoryPurple2 = new Territory("PUPPET SHOW", 10, "Purple");
+	Railway railwayYellow = new Railway("YELLOW RAILWAY LINE", "Yellow");
 	Territory territoryGray1 = new Territory("PUPPET SHOW", 20, "Gray");
 	Territory territoryGray2 = new Territory("PUPPET SHOW", 20, "Gray");
+	Fireworks fireworks = new Fireworks("PAY 5$ TO SEE THE FIREWORKS", 5);
+	Cafe restRoom = new Cafe("REST ROOM");
+	Territory territoryPink1 = new Territory("FERRES WHEEL", 10, "Pink");
+	Territory territoryPink2 = new Territory("CAR RIDE", 15, "Pink");
+	
 	
 	public GameBoard()
 	{	
@@ -17,8 +23,12 @@ public class GameBoard {
 		squares[2] = territoryPurple1;
 		squares[3] = territoryPurple2;
 		squares[4] = chance;
-		squares[5] = territoryGray1;
-		squares[6] = territoryGray2;
+		squares[5] = railwayYellow;
+		squares[6] = territoryGray1;
+		squares[7] = territoryGray2;
+		squares[8] = fireworks;
+		squares[9] = chance;
+		squares[10] = restRoom;
 	}
 	
 	public Square getFierld(int a)
