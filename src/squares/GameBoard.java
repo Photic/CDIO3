50f6b6d5d1bf25db6fbc926716377551aa7ee574
Photic <steen_home@hotmail.com>
2017-11-11 12:45:31 +0100

    Lavet start

--- src/squares/GameBoard.java
+++ src/squares/GameBoard.java
@@ -2,8 +2,17 @@ package squares;
 
 public class GameBoard {
 	
+	Square[] squares = new Square[32];
 	
 	
-
+	public void createBoard()
+	{
+		for (int i = 0; i < squares.length; i++) 
+		{
+			
+			
+			
+		}
+	}
 
 }
