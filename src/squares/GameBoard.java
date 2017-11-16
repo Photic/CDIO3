e1379ab523cbae7d2dde3ced190a85dd86d46116
Photic <steen_home@hotmail.com>
2017-11-16 09:02:13 +0100

    Ændret i Territory og tilføjet leje.

--- src/squares/GameBoard.java
+++ src/squares/GameBoard.java
@@ -17,31 +17,31 @@ public class GameBoard {
 	Square[] squares = new Square[32];
 	Start start = new Start("Start", "Recieve 10$", standard);
 	Chance chance = new Chance("CHANCE", "Pick a chance card", standard);
-	Territory territoryPurple1 = new Territory("BALLOON STAND", "10", purple);
-	Territory territoryPurple2 = new Territory("PUPPET SHOW", "10", purple);
+	Territory territoryPurple1 = new Territory("BALLOON STAND", "10", purple, "100");
+	Territory territoryPurple2 = new Territory("PUPPET SHOW", "10", purple, "100");
 	Railway railwayYellow = new Railway("YELLOW RAILWAY", "Player again", yellow);
-	Territory territoryGray1 = new Territory("PUPPET SHOW", "20", grey);
-	Territory territoryGray2 = new Territory("PUPPET SHOW", "20", grey);
+	Territory territoryGray1 = new Territory("PUPPET SHOW", "20", grey, "100");
+	Territory territoryGray2 = new Territory("PUPPET SHOW", "20", grey, "100");
 	Fireworks fireworks = new Fireworks("FIREWORKS", "Pay 5$", standard);
 	RestRoom restRoom = new RestRoom("REST ROOM", "Wait a turn", standard);
-	Territory territoryPink1 = new Territory("FERRES WHEEL", "10", pink);
-	Territory territoryPink2 = new Territory("CAR RIDE", "15", pink);
+	Territory territoryPink1 = new Territory("FERRES WHEEL", "10", pink, "100");
+	Territory territoryPink2 = new Territory("CAR RIDE", "15", pink, "100");
 	Railway railwayGreen = new Railway("GREEN RAILWAY", "ROLL AGAIN", green);
-	Territory territoryYellow1 = new Territory("HORROR HOUSE", "15", yellow);
-	Territory territoryYellow2 = new Territory("POPCORN STAND", "15", yellow);
+	Territory territoryYellow1 = new Territory("HORROR HOUSE", "15", yellow, "100");
+	Territory territoryYellow2 = new Territory("POPCORN STAND", "15", yellow, "100");
 	GiftShop unclePennybag = new GiftShop("Uncle pennybags", "0", standard);
-	Territory territoryRed1 = new Territory("CONVENIENCE STORE", "15", red);
-	Territory territoryRed2 = new Territory("MINI GOLF", "15", red);
+	Territory territoryRed1 = new Territory("CONVENIENCE STORE", "15", red, "100");
+	Territory territoryRed2 = new Territory("MINI GOLF", "15", red, "100");
 	Railway railwayBlue = new Railway("BLUE RAILWAY", "ROLL AGAIN", blue);
-	Territory territoryOrange1 = new Territory("LOOP THE LOOP", "15", orange);
-	Territory territoryOrange2 = new Territory("ROLLER COASTER", "15", orange);
+	Territory territoryOrange1 = new Territory("LOOP THE LOOP", "15", orange, "100");
+	Territory territoryOrange2 = new Territory("ROLLER COASTER", "15", orange, "100");
 	WaterShow watershow = new WaterShow("WATER SHOW", "Pay 2$", standard);
 	Tax tax = new Tax("TAX TO RESTROOM", "Pay 3$ to the restroom", standard);
-	Territory territoryGreen1 = new Territory("MAGIC SHOW", "15", green);
-	Territory territoryGreen2 = new Territory("OCTUPUS RIDE", "15", green);
+	Territory territoryGreen1 = new Territory("MAGIC SHOW", "15", green, "100");
+	Territory territoryGreen2 = new Territory("OCTUPUS RIDE", "15", green, "100");
 	Railway railwayRed = new Railway("RED RAILWAY", "ROLL AGAIN", red);
-	Territory territoryBlue1 = new Territory("MOVIE HOUSE", "15", blue);
-	Territory territoryBlue2 = new Territory("CHANGE STAGE", "15", blue);
+	Territory territoryBlue1 = new Territory("MOVIE HOUSE", "15", blue, "100");
+	Territory territoryBlue2 = new Territory("CHANGE STAGE", "15", blue, "100");
 
 	
 	public GameBoard()
