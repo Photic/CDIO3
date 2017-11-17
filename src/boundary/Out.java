b25502afdc19a9a220ede52a771d03dd9ae1fe3e
Photic <steen_home@hotmail.com>
2017-11-17 09:22:02 +0100

    changes in out

--- src/boundary/Out.java
+++ src/boundary/Out.java
@@ -13,19 +13,24 @@ public class Out {
 	
 	public void standardOut(String[] a)
 	{
-			System.out.println("--------------------------------------------------------");
+			System.out.println(" ------------------------------------------------------ ");
 			int k = 0;
-			for (int i = 0; i < a.length; i++) {
-				for (int j = 0; j < 30; j++) 
+			
+			for (int i = 0; i < a.length; i++) 
+			{
+				int lengthOfString = 60;
+				for (int j = 0; j < lengthOfString; j++) 
 				{
-					switch (j) {
+					switch (j) 
+					{
 					case 1:
 						System.out.print("|");
 						break;
-					case 5:
+					case 3:
+						j += a[k].length();
 						System.out.print(a[k]); k++;
 						break;
-					case 29:
+					case 55:
 						System.out.print("|");
 						break;
 					default:
@@ -35,44 +40,53 @@ public class Out {
 				}
 				System.out.println("");
 			}
-			
+			System.out.println(" ------------------------------------------------------ ");
 	}
 	
-	public void welcomeNew() {
-		s.println("Welcome to the game.");
+	public void welcomeNew() 
+	{
+		String[] welcome = 	
+			{
+				"Welcome to Menopoly Junior", 
+				"Please enter a number fo players from 2-6"
+			};
+		standardOut(welcome);
 	}
 	
-	public void howManyPlayers() {
-		s.println("How many players are you? 2-6");
+	public void playerCount(int n) 
+	{
+		String[] count =
+			{
+				"Ahhh, you are:"+n,
+				"players, nice!",
+				"Please enter player names"
+			};
+		standardOut(count);
 	}
 	
-	public void playerCount(int n) {
-		s.println("Ahhh, you are " + n + " players, nice!");
-	}
-	
-	public void askForName(int playerNumber) {
+	public void askForName(int playerNumber) 
+	{
 		s.println("What are player " + playerNumber + "'s name?");
 	}
 	
-	public void printName(int playerNumber, String name) {
+	public void printName(int playerNumber, String name) 
+	{
 		s.println("Player " +playerNumber + "'s name is: " + name);
 	}
-	public void printLine() {
+	public void printLine() 
+	{
 		s.println("");
 	}
 	
-	public void printStraigthLine() {
-		s.println("------------------------------------------------------------------------");
-	}
 	
-	
-	public void printPlayerSummary(String[] names) {
+	public void printPlayerSummary(String[] names) 
+	{
+		
 		s.println("The players are:");
 		s.println("");
 		
 		for (int i = 1; i<names.length+1;i++)
 			s.println("Player " + i + ": " + names[i-1]);
-		
 	}
 	
 	
