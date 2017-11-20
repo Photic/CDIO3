8cfab62456eeef0184f1faa36198d3755f7fbc30
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-20 10:45:45 +0100

    Testet Rulebook.Startmoney

---
+++ src/Tests/RuleBook_Test.java
@@ -0,0 +1,33 @@
+package Tests;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import gamelogic.RuleBook;
+
+public class RuleBook_Test {
+
+	
+	@Test
+	public void rBookStartMoney(){
+		RuleBook rulebook = new RuleBook();
+		assertTrue(rulebook.startMoney(2) == 20);
+		assertTrue(rulebook.startMoney(3) == 18);
+		assertTrue(rulebook.startMoney(4) == 16);
+		assertTrue(rulebook.startMoney(387) == 20);
+		}
+	
+	@Test public void newTest(){
+		
+		
+	}	
+	
+	
+	}
+	
+	
+
+
