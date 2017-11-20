f9136beab60d6c2ab01b05666e5de12694150fc9
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-20 14:36:28 +0100

    t

--- src/Tests/Test_Gamelogic.java
+++ src/Tests/Test_Gamelogic.java
@@ -15,6 +15,7 @@ public class Test_Gamelogic {
 	// Her testes metoden til at rykke fra et felt til et andet. 
 	@Test
 	public void newPosition_Test(){
+		Player player = new Player("name", 10);
 		GameLogic gl = new GameLogic();
 		assertTrue(gl.newPosition(3, 6, 24) == 9); //Tester for at rykke 6 felter frem
 		assertTrue(gl.newPosition(23, 6, 24) == 5); //Tester for at nå max antal felter, at gå tilbage til 1 og fortsætte derfra
