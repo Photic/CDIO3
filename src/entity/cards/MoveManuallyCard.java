b2e269da33ac6f8d7d4a6d14437784294f386822
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2017-11-24 10:01:48 +0100

    udkommenteret chancekortet som ikke fungerer

--- src/entity/cards/MoveManuallyCard.java
+++ src/entity/cards/MoveManuallyCard.java
@@ -1,24 +1,20 @@
 package entity.cards;
 
-import boundary.Keyboard;
-import boundary.Out;
-import entity.Player;
-
 public class MoveManuallyCard extends Card{
 	
 	public MoveManuallyCard() {
 		super();
 	}
 	
-	Out out = new Out();
-	Keyboard keyboard = new Keyboard();
-	
-	public void useCard(Player p) {
-		out.moveManuallyCard();
-		int playerChoice = keyboard.getIntRange(1, 5); 
-		p.setPosition(p.getPosition() + playerChoice);
-		
-	}
+//	Out out = new Out();
+//	Keyboard keyboard = new Keyboard();
+//	
+//	public void useCard(Player p) {
+//		out.moveManuallyCard();
+//		int playerChoice = keyboard.getIntRange(1, 5); 
+//		p.setPosition(p.getPosition() + playerChoice);
+//		
+//	}
 	
 	
 }
