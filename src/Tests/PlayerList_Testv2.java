78f231eef79dff2f44459f6feb38d4438a1e0579
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-25 13:17:02 +0100

    Test1_Done

--- src/Tests/PlayerList_Testv2.java
+++ src/Tests/PlayerList_Testv2.java
@@ -7,19 +7,17 @@ import entity.PlayerList;
 public class PlayerList_Testv2 {
 
 	@Test
-	public void test() {
+	public void playerList_Test() {
 		boolean actual = false;
 		boolean expected = true;
 		String[] names = {"Stephan","Mathias","Steen"};
 		PlayerList plist = new PlayerList(3, names, 10);
-		for(int i = 0; i<=plist.getLength(); i++){
-			if (plist.getSpecificPlayer(i).getName()==names[i]){
+		for(int i = 0; i<=plist.getLength()-1; i++){
+			if (plist.getSpecificPlayer(0).getName()==names[0]){
 				actual = true;
 			}
 			assertTrue(actual == expected);
 		}
-
-
 	}
 
 }
