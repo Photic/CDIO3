99b6aa17558ae70de4e93924db9527fa97372e16
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-25 13:56:01 +0100

    cm

--- src/Tests/PlayerList_Testv2.java
+++ src/Tests/PlayerList_Testv2.java
@@ -31,12 +31,12 @@ public class PlayerList_Testv2 {
 		PlayerList plist = new PlayerList(3, names, 10);
 		for(int i = 0; i<plist.getLength(); i++){
 			System.out.println("plads "+ i +" " +plist.getSpecificPlayer(i).getName());
-			}
-		
+		}
+
 		plist.getSpecificPlayer(1).setDead(true);
 		plist.removePlayerIfDead2(plist.getSpecificPlayer(1), plist);
 		for(int i = 0; i<=plist.getLength(); i++){
-		System.out.println("plads "+ i +" " +plist.getSpecificPlayer(i).getName());
+			System.out.println("plads "+ i +" " +plist.getSpecificPlayer(i).getName());
 		}
 	}
 	
