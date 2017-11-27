4fb90d66d6354d8363d33931c42d0d79130d9c71
Photic <steen_home@hotmail.com>
2017-11-27 15:04:01 +0100

    Prøver stadig at lave test af gamecontroller.

--- src/Tests/Test_GameController.java
+++ src/Tests/Test_GameController.java
@@ -1,36 +1,16 @@
 package Tests;
-import junit.runner.Version;
-import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
 import controller.GameController;
-import entity.PlayerList;
 
 public class Test_GameController {
 
-	@Before
-	public void setUp() throws Exception 
-	{
-		new GameController().gameControl();
-		String[] names = {"Stephan","Mathias","Steen"};
-		PlayerList plist = new PlayerList(3, names, 10);
-	}
-	
-	@After
-	public void tearDown() throws Exception 
-	{
-		
-	}
 
 	@Test
 	public void test() 
 	{
-
-
-		System.out.println("JUnit version is: " + Version.id());
+		GameController gc = new GameController();
+		System.setIn(System.in);
 	}
-
+	
 }
