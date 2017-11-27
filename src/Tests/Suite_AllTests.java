b87af29186456ae1c88ac9ec0fd6791ce2be7591
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-27 10:55:38 +0100

    Test Suite

---
+++ src/Tests/Suite_AllTests.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ dieTests.class, PlayerList_Testv.class, RuleBook_Test.class, Test_Gamelogic.class })
+public class Suite_AllTests {
+
+}
