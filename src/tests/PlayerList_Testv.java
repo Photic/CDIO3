bb2f1ae8fec3f8eb372436725f5a56a3c2f4320f
TheyCallMeOii <Stephankhader@gmail.com>
2017-11-28 11:48:52 +0100

    Slettet overflødig test som ikke viker

--- src/tests/PlayerList_Testv.java
+++ src/tests/PlayerList_Testv.java
@@ -21,32 +21,5 @@ public class PlayerList_Testv {
 			assertTrue(actual == expected);
 		}
 	}
-
-	
-	@Test
-	public void removePlayerIfDead(){
-		boolean actual = false;
-		boolean expected = true;
-		String[] names = {"Stephan","Mathias","Steen"};
-		PlayerList plist = new PlayerList(3, names, 10);
-		for(int i = 0; i<plist.getLength(); i++){
-			System.out.println("plads "+ i +" " +plist.getSpecificPlayer(i).getName());
-		}
-		plist.getSpecificPlayer(1).setDead(true);
-		for(int i = 0; i<=plist.getLength()-2; i++){
-			System.out.println("plads "+ i +" " +plist.getSpecificPlayer(i).getName());
-		}
-		if(plist.getSpecificPlayer(1).getName()==names[2]){
-			actual =true;
-		}
-		else{
-			System.out.println("Nope");
-		}
-		
-		assertTrue(actual == expected);
-	}
-	
-
-	
 	
 }
