9453588c1630becd2ea252570b9d7f1ad85d796f
Photic <steen_home@hotmail.com>
2017-12-08 09:33:12 +0100

    Gider ikke rette alle fejl :D

--- src/tests/AllTest.java
+++ src/tests/AllTest.java
@@ -7,5 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ DieTests.class, RuleBook_Test.class, Test_Gamelogic.class, PlayerTest.class, TerritoryTest.class, OutTest.class})
 public class AllTest {
-
+	/**
+	 * Skal bruges til at starte JUnit.
+	 */
 }
